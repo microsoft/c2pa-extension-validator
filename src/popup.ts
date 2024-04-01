ddee8f2a005bd4469ce8da9e82d1c4305e12bdc9
Christian Paquin <cpaquin@microsoft.com>
2024-04-01 15:04:34 -0400

    Get version from package.json instead of version.json

--- src/popup.ts
+++ src/popup.ts
@@ -4,7 +4,7 @@
 */
 
 import { type TrustList, type TrustListInfo, getTrustListInfosRemote, addTrustListRemote, removeTrustListRemote } from './trustlist.js'
-import versionFile from '../version.json'
+import packageManifest from '../package.json'
 
 console.debug('popup.js: load')
 
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function (): void {
   // Update the version number
   const versionElement = document.getElementById('version')
   if (versionElement !== null) {
-    versionElement.textContent = versionFile.version;
+    versionElement.textContent = packageManifest.version;
   }
 
   // Add event listeners to switch tabs
