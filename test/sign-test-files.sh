b01dc42c236ba6e598d05a1901c473c31d0b14e5
Christian Paquin <cpaquin@microsoft.com>
2024-04-03 14:38:25 -0400

    Created test assets

---
+++ test/sign-test-files.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# generate the test certs
+./generate-cert-chain.sh
+
+cert_types=("trusted" "untrusted")
+extensions=("jpg" "png" "mov" "mp4")
+
+# sign the test files
+for cert_type in "${cert_types[@]}"; do
+    certdir="./${cert_type}" # This specifies the certificate directory.
+    echo "Signing test files using the $certdir certificates"
+
+    # copy the test manifest file into the cert directory
+    cp manifest.json $certdir/
+
+    for ext in "${extensions[@]}"; do
+        sourceFile="media/cards.$ext"
+        outputFile="media/cards_${cert_type}.$ext" # Modified to use cert_type directly
+        echo "Signing $sourceFile with the $certdir/manifest.json, output to $outputFile"
+
+        # Sign the test files
+        c2patool $sourceFile -m $certdir/manifest.json -o $outputFile -f
+    done
+done
